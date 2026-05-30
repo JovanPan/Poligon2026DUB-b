9fb8d0bffd9d533f3ee58a0eb0bf9aa221b63c5d
prvabeogim <prvabeogim@outlook.com>
2026-05-30 12:26:59 +0200

    v0.2--n

--- Form1.cs
+++ Form1.cs
@@ -12,45 +12,53 @@ namespace Poligon2026DUB_b
 {
     public partial class Form1 : Form
     {
+        private readonly List<Tacka> _points = new List<Tacka>();
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Graphics g = panel1.CreateGraphics();
-            Tacka t = new Tacka(1, 2);
-            Crtaj(g,t);
-        }
-        public void Crtaj(Graphics dr, Tacka t) 
-        {
-           
+            
+            panel1.Paint += Panel1_Paint;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+            AddPoint(new Tacka(100, 100));
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
+            Tacka prva = new Tacka(1, 1);
+            AddPoint(prva);
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
 
+        public void AddPoint(Tacka t)
+        {
+            _points.Add(t);
+            panel1.Invalidate();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void Panel1_Paint(object sender, PaintEventArgs e)
         {
+            foreach (var t in _points)
+            {
+                Crtaj(e.Graphics, t);
+            }
+        }
 
+        public void Crtaj(Graphics dr, Tacka t)
+        {
+            int panelHeight = panel1.ClientSize.Height;
+            int x = (int)(t.x - 2);
+            int y = panelHeight - (int)t.y - 2;
+
+            using (var cetka = new SolidBrush(Color.Red))
+            {
+                dr.FillEllipse(cetka, x, y, 8, 8);
+            }
         }
     }
 }
